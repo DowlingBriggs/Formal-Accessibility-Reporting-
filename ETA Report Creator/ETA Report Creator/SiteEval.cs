66fca391016e8128164e7eb13c3d621b35419013
ETA Etime <ETA Etime@DESKTOP-QT85IIB>
2017-12-07 13:44:07 -0600

    Sublist figured out

--- ETA Report Creator/ETA Report Creator/SiteEval.cs
+++ ETA Report Creator/ETA Report Creator/SiteEval.cs
@@ -20,8 +20,8 @@ namespace ETA_Report_Creator
             InitializeComponent();
 
             eval_siteName.Text = ReportCreator.siteName;
-           // eval_testerName.Text = ReportCreator.testerName; --- not needed
-          
+            // eval_testerName.Text = ReportCreator.testerName; --- not needed
+
         }
 
         private void siteEval_exit_Click(object sender, EventArgs e)
@@ -34,18 +34,18 @@ namespace ETA_Report_Creator
         {
             //Check to make sure all radio box groups are checked -- if so; write code
             bool validate = validateCB();
-            if(validate == true)
-                {
+            if (validate == true)
+            {
                 //Write word doc
                 CreateDocument();
 
-                }
+            }
             else
-                {
+            {
                 MessageBox.Show("Please select Pass/Fail for all criteria", "Missing Selection",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            
+            }
+
         }
 
         //Create document method
@@ -61,7 +61,7 @@ namespace ETA_Report_Creator
 
                 //Set status for word application is to be visible or not.
                 winword.Visible = false;
-                
+
                 //Create a missing variable for missing value
                 object missing = System.Reflection.Missing.Value;
 
@@ -88,7 +88,7 @@ namespace ETA_Report_Creator
                 title1.Range.Font.Size = 22;
                 title1.Range.Font.Name = "Candara";
                 title1.Range.Borders[WdBorderType.wdBorderTop].LineStyle = WdLineStyle.wdLineStyleDot;
-                title1.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;                
+                title1.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
                 title1.Range.InsertParagraphAfter();
 
                 //Report title part 2
@@ -101,7 +101,7 @@ namespace ETA_Report_Creator
                 title2.Range.Borders[WdBorderType.wdBorderBottom].LineStyle = WdLineStyle.wdLineStyleDot;
                 title2.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
                 title2.Range.InsertParagraphAfter();
-                
+
                 //Report title Tester Name
                 Paragraph title3 = document.Content.Paragraphs.Add(ref missing);
                 title3.Range.Text = "Submitted by " + ReportCreator.testerName;
@@ -109,7 +109,7 @@ namespace ETA_Report_Creator
                 title3.Range.Font.Name = "Candara";
                 title3.Range.Font.Italic = 1;
                 title3.Range.Font.ColorIndex = WdColorIndex.wdBlack;
-                title3.Range.Font.Size = 11;                
+                title3.Range.Font.Size = 11;
                 title3.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
                 title3.Range.InsertParagraphAfter();
 
@@ -135,7 +135,7 @@ namespace ETA_Report_Creator
                 string day = DateTime.Now.Day.ToString();
                 string month = DateTime.Now.ToString("MMMM");
                 string year = DateTime.Now.Year.ToString();
-                title3.Range.Text = day+" "+month+" "+year;
+                title3.Range.Text = day + " " + month + " " + year;
                 title3.Range.Font.Name = "Candara";
                 title3.Range.Font.Italic = 1;
                 title3.Range.Font.ColorIndex = WdColorIndex.wdBlack;
@@ -161,10 +161,10 @@ namespace ETA_Report_Creator
                 string pagesTested = ReportCreator.pagesTested;
                 summary2.Range.Text = "This report summarizes the accessibility review of University of Alabama Adapted Athletics web pages. "
                     + pagesTested
-                    +" pages were examined using a checklist derived from the World Wide Web Consortium Web Content Accessibility Guidelines, the emerging standard for web accessibility."
-                    +" The sites/pages were reviewed in Fall 2017 by the Office of Information Technology Emerging Technology and Accessibility (ETA) team."
-                    +" It is hoped that this initial evaluation will offer insight into the accessibility of these UA web pages and suggest future steps to"
-                    +" improve the accessibility of the Office of Information Technology’s web presence. ";
+                    + " pages were examined using a checklist derived from the World Wide Web Consortium Web Content Accessibility Guidelines, the emerging standard for web accessibility."
+                    + " The sites/pages were reviewed in Fall 2017 by the Office of Information Technology Emerging Technology and Accessibility (ETA) team."
+                    + " It is hoped that this initial evaluation will offer insight into the accessibility of these UA web pages and suggest future steps to"
+                    + " improve the accessibility of the Office of Information Technology’s web presence. ";
                 summary2.Range.Font.ColorIndex = WdColorIndex.wdBlack;
                 summary2.Range.Font.Size = 11;
                 summary2.Range.Font.Name = "Candara";
@@ -187,8 +187,8 @@ namespace ETA_Report_Creator
                 //Results paragraph
                 Paragraph results2 = document.Content.Paragraphs.Add(ref missing);
                 results2.Range.Text = "Like many UA web resources, the site does contain issues that can prevent users with disabilities from accessing site contents and functions."
-                    +" The results given here summarize the review findings, focusing on the challenges that would interfere most heavily with a user’s experience."
-                    +" Individual page checklists are available. ";
+                    + " The results given here summarize the review findings, focusing on the challenges that would interfere most heavily with a user’s experience."
+                    + " Individual page checklists are available. ";
                 results2.Range.Font.ColorIndex = WdColorIndex.wdBlack;
                 results2.Range.Font.Size = 11;
                 results2.Range.Font.Name = "Candara";
@@ -224,14 +224,14 @@ namespace ETA_Report_Creator
                 //Created bulleted list
                 Paragraph assets = document.Content.Paragraphs.Add();
                 assets.Range.ListFormat.ApplyBulletDefault();
-                
-                    for (int i = 0; i < bulletItems.Length; i++)
-                    {
-                        string bulletItem = bulletItems[i];
-                        if (i < bulletItems.Length - 1)
-                            bulletItem = bulletItem + "\n";
-                        assets.Range.InsertBefore(bulletItem);
-                    }
+
+                for (int i = 0; i < bulletItems.Length; i++)
+                {
+                    string bulletItem = bulletItems[i];
+                    if (i < bulletItems.Length - 1)
+                        bulletItem = bulletItem + "\n";
+                    assets.Range.InsertBefore(bulletItem);
+                }
 
 
 
@@ -264,7 +264,8 @@ namespace ETA_Report_Creator
                 if (rb_f3.Checked == true)
                 {
                     nextStepsList.Add("Color and Contrast: Make sure that links change visibly when a user hovers over or tabs to them. Links should be underlined and color change shouldn't be only indicator to distinguish elements.")
-;                }
+;
+                }
                 if (rb_f4.Checked == true)
                 {
                     //resultsList.Add("");
@@ -276,7 +277,7 @@ namespace ETA_Report_Creator
                 //Created bulleted list
                 Paragraph nextSteps = document.Content.Paragraphs.Add();
                 nextSteps.Range.ListFormat.ApplyBulletDefault();
-          
+
                 for (int i = 0; i < bulletItems_nextSteps.Length; i++)
                 {
                     string bulletItem_nextSteps = bulletItems_nextSteps[i];
@@ -286,7 +287,7 @@ namespace ETA_Report_Creator
                 }
 
 
-                
+
                 //Testing Process Section
 
                 //Testing Process Title
@@ -301,50 +302,54 @@ namespace ETA_Report_Creator
 
                 //Process paragraph
                 Paragraph process2 = document.Content.Paragraphs.Add(ref missing);
-                process2.Range.Text = "The AMP automated tool (Accessibility Management Platform) was used as an initial evaluation of page accessibility to find potential errors and alerts related to WCAG 2.0 A/AA." 
-                    +" Each page was then checked manually based on 37 criteria, summarized below, and status documented as Pass, Fail with explanation, or N/A (not applicable)." 
-                    +" Pages were evaluated by at least two individuals from the Center for Instructional Technology Emerging Technology and Accessibility team." 
-                    +" Checklists for each page, which include details regarding accessibility issues, are available. ";
+                process2.Range.Text = "The AMP automated tool (Accessibility Management Platform) was used as an initial evaluation of page accessibility to find potential errors and alerts related to WCAG 2.0 A/AA."
+                    + " Each page was then checked manually based on 37 criteria, summarized below, and status documented as Pass, Fail with explanation, or N/A (not applicable)."
+                    + " Pages were evaluated by at least two individuals from the Center for Instructional Technology Emerging Technology and Accessibility team."
+                    + " Checklists for each page, which include details regarding accessibility issues, are available. ";
                 process2.Range.Font.ColorIndex = WdColorIndex.wdBlack;
                 process2.Range.Font.Size = 11;
                 process2.Range.Font.Name = "Candara";
                 process2.Range.InsertParagraphAfter();
 
 
-                //Process List
-    
-                
-                                Paragraph p1 = document.Content.Paragraphs.Add();
-                                Range range = document.Range(0,0);
-                                p1.Range.ListFormat.ApplyBulletDefault();
-                                p1.Range.Text = "Birinci";
-                                p1.Range.ListFormat.ApplyBulletDefault();
-                                p1.Range.InsertParagraphAfter();
-                                ListTemplate listTemplate = p1.Range.ListFormat.ListTemplate;
+                //Process List                
+                Paragraph p1 = document.Content.Paragraphs.Add();
+                Range range = document.Range(0, 0);
+                p1.Range.ListFormat.ApplyBulletDefault();
+                p1.Range.Text = "Keyboard accessibility:";
+                p1.Range.ListFormat.ApplyBulletDefault();
+                p1.Range.InsertParagraphAfter();
+                ListTemplate listTemplate = p1.Range.ListFormat.ListTemplate;
 
-                             //   Paragraph p2 = document.Content.Paragraphs.Add();
-                                Range subRange = document.Range(p1.Range.StoryLength - 1, p1.Range.StoryLength -1);
-                                subRange.ListFormat.ApplyBulletDefault();
-                                subRange.ListFormat.ListIndent();
-                                subRange.Text = ">" + "Alt Birinci";
-                                subRange.InsertParagraphAfter();
-                                ListTemplate sublistTemplate = subRange.ListFormat.ListTemplate;
+                //Paragraph p2 = document.Content.Paragraphs.Add();
+                Range subRange = document.Range(p1.Range.StoryLength - 1, p1.Range.StoryLength - 1);
+                //subRange.ListFormat.ApplyBulletDefault();
+                subRange.ListFormat.ListIndent();
+                subRange.Text = "All functionality, including forms, dialog boxes, and pop-ups, is available using only the keyboard(tab, shift + tab, enter, etc.).\n"
+                    + @"A ""skip navigation"" link is available.";
+                ListTemplate sublistTemplate = subRange.ListFormat.ListTemplate;
+                p1.Range.InsertParagraphAfter();
 
 
-                /*
-                                     Range subRange2 = document.Range(p1.Range.StoryLength - 2, p1.Range.StoryLength - 2);
-                                    subRange2.ListFormat.ApplyListTemplate(sublistTemplate);
-                                     subRange2.ListFormat.ListIndent();
-                                    subRange2.Text = ">" + "Alt İkinci";
-                                    subRange2.InsertParagraphAfter();
-                    */
 
-                /* Range range2 = document.Range(p1.Range.StoryLength - 1);
-                 range2.ListFormat.ApplyListTemplateWithLevel(listTemplate, true);
-                 WdContinue isContinue = range2.ListFormat.CanContinuePreviousList(listTemplate);
-                 range2.Text = "İkinci";
-                 range2.InsertParagraphAfter();
-                 */
+                /* Range subRange2 = document.Range(p1.Range.StoryLength - 2, p1.Range.StoryLength - 2);
+                                     subRange2.ListFormat.ApplyListTemplate(sublistTemplate);
+                 subRange2.InsertParagraphAfter();
+                 p1.Range.InsertParagraphAfter();
+                 subRange2.ListFormat.ListIndent();
+                                     subRange2.Text = "Alt İkinci";
+                                     subRange2.InsertParagraphAfter();
+                                     */
+
+
+                Range range2 = document.Range(p1.Range.StoryLength - 1);
+                range2.ListFormat.ApplyListTemplateWithLevel(listTemplate, true);
+                WdContinue isContinue = range2.ListFormat.CanContinuePreviousList(listTemplate);
+                range2.Text = "İkinci";
+                range2.InsertParagraphAfter();
+
+
+
 
 
                 //Federal Action Regarding Accessibility in Higher Education Section
@@ -376,7 +381,7 @@ namespace ETA_Report_Creator
                 Paragraph fedActionList = document.Content.Paragraphs.Add();
 
                 fedActionList.Range.ListFormat.ApplyBulletDefault();
-                string[] fedBulletItems = new string[] 
+                string[] fedBulletItems = new string[]
                 {   "Florida State University (June 2014)",
                     "University of Montana-Missoula (March 2014)",
                     "Louisiana Tech (July 2013)",
@@ -418,7 +423,7 @@ namespace ETA_Report_Creator
                 string mnth = DateTime.Now.Month.ToString();
                 string yr = DateTime.Now.Year.ToString();
                 object filename = docfolder + "\\" + ReportCreator.siteName + "_" + mnth + "_" + yr;
-                    
+
                 document.SaveAs2(ref filename);
                 document.Close(ref missing, ref missing, ref missing);
                 document = null;
